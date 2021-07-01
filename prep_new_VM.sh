e47da86be4828abaac0f66368169c8ee86f6a409
jake1512 <dokhoinguyen.nguyen@gmail.com>
2021-07-02 00:55:02 +0700

    Update prep_new_VM.sh

--- prep_new_VM.sh
+++ prep_new_VM.sh
@@ -4,6 +4,7 @@ wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
 echo "kali" | sudo apt install ./vscode.deb
 sudo -S apt update
 sudo apt install terminator -y
-sudo apt install pwncat -y
+sudo apt install python3-pip
+pip install git+https://github.com/calebstewart/pwncat.git
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
