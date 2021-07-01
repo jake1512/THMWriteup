51a25b54d4c937b2bfe769bf8bbcc2997d4e97f9
jake1512 <dokhoinguyen.nguyen@gmail.com>
2021-07-02 00:51:42 +0700

    Update prep_new_VM.sh

--- prep_new_VM.sh
+++ prep_new_VM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
-sudo apt install ./vscode.deb
-echo "kali" | sudo -S apt update
+echo "kali" | sudo apt install ./vscode.deb
+sudo -S apt update
 sudo apt install terminator -y
 sudo apt install pwncat -y
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
